83202c6383e602d8ccd158b9f4b6bc1405486819
bozzyphat <92072531+bozzyphat@users.noreply.github.com>
2023-05-25 13:19:25 +1200

    Update the restore wordpress web app

--- wordpress/readme.md
+++ wordpress/readme.md
@@ -1,3 +1,4 @@
+Create Wordpress app service on Azure 
 Stage 1:
     Install the Azure CLI and docker
 
@@ -11,22 +12,22 @@ Stage 2:
     
     In this script, there is a step you have to key in the password you will be using to access the databse in future
 
-
 Stage 3:
     Run the PowerShell script with Admin privileges:
-    This script will set up WordPress using docker
+    This script will set up WordPress on Azure web app using docker
         .\create-wordpress-docker-azure.ps1
 
 
-Stage 4:
+
+Backup and restore MYSQl databse
+Stage 1:
     *****Make sure you type in the correct date format to restore the databse, point-in-time (in UTC) to restore from, use the ISO8601 format with your local timezone: 2023-05-24T05:59:00Z****
 
     Run the PowerShell script with Admin privileges:
     This script will restore the original MYSQL database to new databse sever within the backup retention time (7days) 
         .\restore-mysql-azure.ps1
 
-
-Stage 5:
+Stage 2:
     Run the PowerShell script with Admin privileges:
     This script will allow you to chaneg the WordPress database to the newly created restore databse server
         .\change-wordpress-databse.ps1
---
+++ wordpress/restore-wordpress-azure.ps1
@@ -0,0 +1,35 @@
+#Requires -RunAsAdministrator
+
+function Green
+{
+    process { Write-Host $_ -ForegroundColor Green }
+}
+
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+
+Write-Output "Login with Azure account" | Green
+az login 
+
+# Variable block
+$location ="australiaeast"
+$resourceGroup ="rg-wp-dev-auseast-001"
+$login ="azureuser"
+
+
+
+
+#Variable to get the list of current web app
+Write-Output "Current list of web app in the Resrouces group $resourceGroup" | Green
+az webapp list -o table
+$appname=Read-Host 'Type in the web app name will be restore'  
+
+#Variable to get the list of current web app restore points 
+Write-Output "Current list of restore points for web app $appname " | Green
+az webapp config snapshot list --name $appname --resource-group $resourceGroup -o table
+$restoretime=Read-Host 'Type in the selected snapshot time to be restore, follow with the result format 2023-05-24T22:45:17.0054237'  
+
+#Restore the web app
+Write-Output "Restore web app $appname to the restore point in time $restoretime" | Green
+az webapp config snapshot restore --name $appname --resource-group $resourceGroup --time $restoretime
